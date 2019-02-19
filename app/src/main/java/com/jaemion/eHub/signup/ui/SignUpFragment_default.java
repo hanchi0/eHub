40d9f238d5050d0129e971a236285b9556e3edf1
hjm98 <hjm9807@gmail.com>
2019-02-19 18:04:15 +0900

    1. SignUpFragment_default databinding & LiveData 적용

--- app/src/main/java/com/jaemion/eHub/signup/ui/SignUpFragment_default.java
+++ app/src/main/java/com/jaemion/eHub/signup/ui/SignUpFragment_default.java
@@ -35,11 +35,10 @@ import com.jaemion.eHub.network.NetworkInterface;
 import com.jaemion.eHub.network.model.UserData;
 import com.jaemion.eHub.signup.SignUpActivity;
 
-public class SignUpFragment_default extends Fragment implements View.OnClickListener {
+public class SignUpFragment_default extends Fragment {
     private SignUpViewModel mViewModel;
     SignUpFragmentDefaultBinding binding;
     InputMethodManager inputMethodManager;
-    String pwPattern, pwPattern2;
 
     public static SignUpFragment_default newInstance() {
         return new SignUpFragment_default();
@@ -51,66 +50,7 @@ public class SignUpFragment_default extends Fragment implements View.OnClickList
                              @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.sign_up_fragment_default, container, false);
 
-        pwPattern = "^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-z])(?=.*[A-Z]).{7,15}$";
-        pwPattern2 = "(.)\\1\\1\\1";
 
-        binding.signUpFragmentDefaultEtID.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mViewModel.stId = s.toString();
-                checkButtonAble();
-            }
-        });
-        binding.signUpFragmentDefaultEtPW.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mViewModel.stPw = s.toString();
-                checkButtonAble();
-            }
-        });
-        binding.signUpFragmentDefaultEtPWCheck.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mViewModel.stPwCheck = s.toString();
-
-                if (mViewModel.stPw.equals(mViewModel.stPwCheck))
-                    mViewModel.isMatched = true;
-                else
-                    mViewModel.isMatched = false;
-
-                checkButtonAble();
-            }
-        });
 
         binding.signUpFragmentDefaultInputLayoutId.setCounterEnabled(true);
         binding.signUpFragmentDefaultInputLayoutId.setCounterMaxLength(15);
@@ -119,10 +59,6 @@ public class SignUpFragment_default extends Fragment implements View.OnClickList
         binding.signUpFragmentDefaultInputLayoutPwCheck.setCounterEnabled(true);
         binding.signUpFragmentDefaultInputLayoutPwCheck.setCounterMaxLength(15);
 
-        binding.signUpFragmentDefaultBtnNext.setOnClickListener(this);
-        binding.signUpFragmentDefaultBtnNext.setEnabled(false);
-        binding.signUpFragmentDefaultBtnNext.setBackgroundColor(getResources().getColor(R.color.button_background_disable));
-        binding.signUpFragmentDefaultBtnNext.setTextColor(getResources().getColor(R.color.button_text_disable));
         inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
 
         return binding.getRoot();
@@ -133,6 +69,22 @@ public class SignUpFragment_default extends Fragment implements View.OnClickList
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(getActivity()).get(SignUpViewModel.class);
         // TODO: Use the ViewModel
+        mViewModel.getUserData().observe(this, o -> {
+            if (mViewModel.userType == SignUpFragment_Select.UserType.EMPLOYER.getValue()) {
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .addToBackStack(null)
+                        .setCustomAnimations(R.anim.slide_right_in, R.anim.slide_left_out, R.anim.slide_left_in, R.anim.slide_right_out)
+                        .replace(R.id.signUp_container, SIgnUpFragment_detail_Employer.newInstance())
+                        .commit();
+            } else {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack(null).setCustomAnimations(R.anim.slide_right_in, R.anim.slide_left_out, R.anim.slide_left_in, R.anim.slide_right_out)
+                        .replace(R.id.signUp_container, SignUpFragment_detail_Employee.newInstance())
+                        .commit();
+            }
+        });
+        binding.setViewModel(mViewModel);
     }
 
     @Override
@@ -144,8 +96,7 @@ public class SignUpFragment_default extends Fragment implements View.OnClickList
             binding.signUpFragmentDefaultEtPW.setText(mViewModel.stPw);
         if (mViewModel.stPwCheck != null)
             binding.signUpFragmentDefaultEtPWCheck.setText(mViewModel.stPwCheck);
-        checkButtonAble();
-
+        mViewModel.checkButtonAble();
         ((SignUpActivity) getActivity()).setToolbar("기본 정보 입력");
         ((SignUpActivity) getActivity()).getSupportActionBar().show();
 
@@ -159,59 +110,5 @@ public class SignUpFragment_default extends Fragment implements View.OnClickList
         inputMethodManager.hideSoftInputFromWindow(binding.signUpFragmentDefaultEtPWCheck.getWindowToken(), 0);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.signUp_fragment_default_btnNext:
-                binding.signUpFragmentDefaultBtnNext.setEnabled(false);
-                binding.signUpFragmentDefaultBtnNext.setBackgroundColor(getResources().getColor(R.color.button_background_disable));
-                binding.signUpFragmentDefaultBtnNext.setTextColor(getResources().getColor(R.color.button_text_disable));
-                //mViewModel.createUser(getContext();
-                Call<UserData> call = NetworkInterface.retrofit.create(NetworkInterface.class).createUser(mViewModel.stId, mViewModel.stPw, mViewModel.userType);
-                call.enqueue(new Callback<UserData>() {
-                    @Override
-                    public void onResponse(Call<UserData> call, Response<UserData> response) {
-                        if (response.isSuccessful()) {
-                            UserDataManager.getInstance().setUserData(response.body());
-                            if (mViewModel.userType == SignUpFragment_Select.UserType.EMPLOYER.getValue()) {
-                                getActivity().getSupportFragmentManager().beginTransaction()
-                                        .addToBackStack(null)
-                                        .setCustomAnimations(R.anim.slide_right_in, R.anim.slide_left_out, R.anim.slide_left_in, R.anim.slide_right_out)
-                                        .replace(R.id.signUp_container, SIgnUpFragment_detail_Employer.newInstance())
-                                        .commit();
-                            } else {
-                                getActivity().getSupportFragmentManager()
-                                        .beginTransaction()
-                                        .addToBackStack(null).setCustomAnimations(R.anim.slide_right_in, R.anim.slide_left_out, R.anim.slide_left_in, R.anim.slide_right_out)
-                                        .replace(R.id.signUp_container, SignUpFragment_detail_Employee.newInstance())
-                                        .commit();
-                            }
-                        }
-                    }
 
-                    @Override
-                    public void onFailure(Call<UserData> call, Throwable t) {
-                        binding.signUpFragmentDefaultBtnNext.setEnabled(true);
-                        binding.signUpFragmentDefaultBtnNext.setBackground(getResources().getDrawable(R.drawable.button_ripple_enable));
-                        binding.signUpFragmentDefaultBtnNext.setTextColor(getResources().getColor(R.color.button_text_enable));
-                    }
-                });
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    void checkButtonAble() {
-        if (!mViewModel.stId.isEmpty() && !mViewModel.stPw.isEmpty() && !mViewModel.stPwCheck.isEmpty() && mViewModel.isMatched) {
-            binding.signUpFragmentDefaultBtnNext.setEnabled(true);
-            binding.signUpFragmentDefaultBtnNext.setBackground(getResources().getDrawable(R.drawable.button_ripple_enable));
-            binding.signUpFragmentDefaultBtnNext.setTextColor(getResources().getColor(R.color.button_text_enable));
-        } else {
-            binding.signUpFragmentDefaultBtnNext.setEnabled(false);
-            binding.signUpFragmentDefaultBtnNext.setBackgroundColor(getResources().getColor(R.color.button_background_disable));
-            binding.signUpFragmentDefaultBtnNext.setTextColor(getResources().getColor(R.color.button_text_disable));
-        }
-    }
 }
--- app/src/main/java/com/jaemion/eHub/signup/ui/SignUpViewModel.java
+++ app/src/main/java/com/jaemion/eHub/signup/ui/SignUpViewModel.java
@@ -1,24 +1,51 @@
 package com.jaemion.eHub.signup.ui;
 
+import androidx.databinding.BindingConversion;
+import androidx.databinding.ObservableBoolean;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
+import android.nfc.cardemulation.CardEmulation;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+import com.jaemion.eHub.R;
+import com.jaemion.eHub.datamanager.UserDataManager;
+import com.jaemion.eHub.network.NetworkInterface;
+import com.jaemion.eHub.network.model.UserData;
+import com.jaemion.eHub.signup.SignUpActivity;
 
 public class SignUpViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    private MutableLiveData<UserData> userData;
 
-    String stId, stPw, stPwCheck,stPhone, stBnum, stNick, stCarType, stRadius, stLocation;
+    String stId, stPw, stPwCheck, stPhone, stBnum, stNick, stCarType, stRadius, stLocation, pwPattern, pwPattern2;
     int userType;
-    Image iProfile;
+    Image ivProfile;
 
     Boolean isMatched, isSafe;
+    ObservableBoolean isEnable;
 
     public SignUpViewModel() {
         super();
         stId = stPw = stPwCheck = stPhone = stBnum = stNick = stCarType = stRadius = stLocation = "";
         isMatched = isSafe = false;
+        isEnable = new ObservableBoolean();
+        isEnable.set(false);
+        userData = new MutableLiveData<>();
 
+        pwPattern = "^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-z])(?=.*[A-Z]).{7,15}$";
+        pwPattern2 = "(.)\\1\\1\\1";
     }
 
     @Override
@@ -26,11 +53,73 @@ public class SignUpViewModel extends ViewModel {
         super.onCleared();
     }
 
-    public void createUser(Context context){
-        Toast.makeText(context, "유저 생성 요청", Toast.LENGTH_SHORT).show();
+    public void addAdditionalData(Context context) {
+        Toast.makeText(context, "추가 정보 저장", Toast.LENGTH_SHORT).show();
     }
 
-    public void addAdditionalData(Context context){
-        Toast.makeText(context, "추가 정보 저장", Toast.LENGTH_SHORT).show();
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signUp_fragment_default_btnNext:
+                isEnable.set(false);
+                //mViewModel.createUser(getContext();
+                Call<UserData> call = NetworkInterface.retrofit.create(NetworkInterface.class).createUser(stId, stPw, userType);
+                call.enqueue(new Callback<UserData>() {
+                    @Override
+                    public void onResponse(Call<UserData> call, Response<UserData> response) {
+                        if (response.isSuccessful())
+                            userData.setValue(response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<UserData> call, Throwable t) {
+                        checkButtonAble();
+                    }
+                });
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void onIdTextChanged(CharSequence s, int start, int before, int count) {
+        stId = s.toString();
+        checkButtonAble();
+    }
+
+    public void onPwTextChanged(CharSequence s, int start, int before, int count) {
+        stPw = s.toString();
+        checkButtonAble();
+
+    }
+
+    public void onPwCheckTextChanged(CharSequence s, int start, int before, int count) {
+        stPwCheck = s.toString();
+        if (stPw.equals(stPwCheck))
+            isMatched = true;
+        else
+            isMatched = false;
+        checkButtonAble();
+    }
+    
+    void checkButtonAble() {
+        if (!stId.isEmpty() && !stPw.isEmpty() && !stPwCheck.isEmpty() && isMatched) {
+            isEnable.set(true);
+        } else {
+            isEnable.set(false);
+        }
+    }
+
+    public ObservableBoolean getIsEnable() {
+        return isEnable;
+    }
+
+    public LiveData getUserData() {
+        return userData;
+    }
+
+    @BindingConversion
+    public static ColorDrawable convertColorToDrawable(int color) {
+        return new ColorDrawable(color);
     }
 }
